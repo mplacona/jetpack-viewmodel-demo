0fd453cf159defa905e7954833c2eed42257a5fb
Marcos Placona <marcos.placona@gmail.com>
2019-04-25 10:46:22 +0100

    Commit initial version without lifecycle management

--- app/src/main/java/uk/co/placona/jetpackdemo1/DataSource.kt
+++ app/src/main/java/uk/co/placona/jetpackdemo1/DataSource.kt
@@ -2,6 +2,16 @@ package uk.co.placona.jetpackdemo1
 
 class DataSource {
     private val tag = MainActivity::class.java.simpleName
+    private lateinit var myRandomNumber:Number
 
-    fun getMagicNumber() = (1..42).shuffled().first()
+    private fun createMagicNumber(){
+        myRandomNumber = (1..42).shuffled().first()
+    }
+
+    fun getMagicNumber(): Number {
+        if(!::myRandomNumber.isInitialized){
+            createMagicNumber()
+        }
+        return myRandomNumber
+    }
 }
--- app/src/main/java/uk/co/placona/jetpackdemo1/MainActivity.kt
+++ app/src/main/java/uk/co/placona/jetpackdemo1/MainActivity.kt
@@ -1,18 +1,16 @@
 package uk.co.placona.jetpackdemo1
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var dataSource: DataSource
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dataSource = DataSource()
-        tvMagicNumber.text = dataSource.getMagicNumber().toString()
+        val myData = DataSource()
+        tvMagicNumber.text = myData.getMagicNumber().toString()
     }
 }
